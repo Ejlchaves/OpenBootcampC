d72dcaadd5a1abeb86928f29b85fcd502dee2f80
Chaves Enzo <julian.chaves96@gmail.com>
2023-07-12 18:09:06 -0300

    Exercises 1 and 2 from C# Course

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Ejercicio 1
+Console.WriteLine("Introduce tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Tu nombre es  " + nombre);
+
+//Ejercicio 2
+string hora = DateTime.Now.ToString();
+Console.WriteLine(hora); 
